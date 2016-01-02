862cde90a541c3f0ee11285274eccd45a831bef5
cgrad01 <cgrad01@gmail.com>
2016-01-02 12:48:22 -0500

    limit threads to actual reddit threads

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,10 +1,16 @@
 app.controller('MainController', function($scope, $http) {
   $scope.name = "Sports App";
+  $scope.threads = [];
 
   fetch();
 
   function fetch() {
     $http.get("http://www.reddit.com/r/NYKnicks.json")
-    .success(function(response){$scope.threads = response.data.children;});
+    .success(function(response){
+      for(i=0; i < response.data.children.length; i++){
+        if(response.data.children[i].data.domain === "self.NYKnicks"){
+          $scope.threads.push(response.data.children[i]);
+        }
+      }});
   }
 });
