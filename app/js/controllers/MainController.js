c4705e91961c6a8df7dccb32d24a7b41a44000e8
cgrad01 <cgrad01@gmail.com>
2016-01-03 20:52:01 -0500

    filter threads to reddit domain only

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -2,15 +2,17 @@ app.controller('MainController', function($scope, $http) {
   $scope.name = "Sports App";
   $scope.threads = [];
 
-  fetch();
-
-  function fetch() {
+  function filterThreads() {
     $http.get("http://www.reddit.com/r/NYKnicks.json")
     .success(function(response){
       for(i=0; i < response.data.children.length; i++){
         if(response.data.children[i].data.domain === "self.NYKnicks"){
           $scope.threads.push(response.data.children[i]);
         }
-      }});
-  }
+      }
+    });
+  };
+
+  filterThreads();
+
 });
